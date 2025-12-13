535f165aa16c12166a5b1782d88d777812a65de5
plmex <php.eleuterio@gmail.com>
2025-12-12 22:29:59 -0300

    Exercício 6

---
+++ ex006.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um número: '))
+             
+print("O número digitado foi", numero)
+print("O dobro de {} é".format(numero), numero * 2)
+print("O triplo de {} é".format(numero), numero * 3)
+print("A raiz quadrada de {} é {:.2f}".format(numero, numero ** (1/2)))
