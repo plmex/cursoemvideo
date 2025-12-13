442c21298ab7b8b86168b79013143f4d3356dfc6
plmex <php.eleuterio@gmail.com>
2025-12-12 22:59:08 -0300

    Exercício 8]

---
+++ ex008.py
@@ -0,0 +1,6 @@
+metros = float(input('Digite o valor em metros: '))
+
+centimetros = metros * (10**2)
+milimetros = metros * (10**3)
+
+print('{} metros corresponde a {} centímetros ou {} milímetros' .format(metros, centimetros, milimetros))
