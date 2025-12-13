af1bbddd81af813a68d38bc80c152e157edf5b03
plmex <php.eleuterio@gmail.com>
2025-12-13 20:02:59 -0300

    Exercício 18

---
+++ ex018.py
@@ -0,0 +1,9 @@
+import math
+
+
+angulo = float(input('\nInsira o valor do ângulo: '))
+
+
+print('Sen {}° = {:.2f}'.format(angulo, math.sin(angulo)))
+print('Cos {}° = {:.2f}'.format(angulo, math.cos(angulo)))
+print('Tan {}° = {:.2f}'.format(angulo, math.tan(angulo)))
