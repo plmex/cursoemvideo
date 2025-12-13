97b8fd6a4c4656c2acc86b1f24c25d0121a53a09
plmex <php.eleuterio@gmail.com>
2025-12-13 18:53:05 -0300

    Exercício 13

--- ex013.py
+++ ex013.py
@@ -2,4 +2,4 @@ salario = float(input('Insira o valor do salário atual (R$): '))
 
 
 print('\nParabéns! Você é um ótimo funcionário e por isso recebeu um aumento de 15%!', end = " ")
-print('A partir de hoje o seu salário passa de R$ {:.2f} para R$ {:.2f}\n'. format(salario, salario + salario * 15/100))
+print('A partir de hoje o seu salário passa de R$ {:.2f} para R$ {:.2f}!\n'. format(salario, salario + salario * 15/100))
