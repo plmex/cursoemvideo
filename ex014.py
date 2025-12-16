7777412d0807046e6cb79876672d8f74f5a2990d
plmex <php.eleuterio@gmail.com>
2025-12-15 22:32:35 -0300

    Exercício 14

--- ex014.py
+++ ex014.py
@@ -2,4 +2,4 @@ temperatura = float(input('Informe a temperatura (°C): '))
 
 fahrenheit = (temperatura * 9/5) + 32
 
-print('{} °C equivale a {} °F'. format(temperatura, fahrenheit))
+print('{} °C equivale a {} °F.'. format(temperatura, fahrenheit))
